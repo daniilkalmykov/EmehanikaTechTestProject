a215e7a1d3d471ac3594dd368ea01d16a8c8d184
daniilkalmykov <daniilk24960@gmail.com>
2023-09-11 21:26:40 +0600

    set torch to scene

--- Assets/Source/CompositeRoot/CompositeOrder.cs
+++ Assets/Source/CompositeRoot/CompositeOrder.cs
@@ -5,10 +5,12 @@ namespace Source.CompositeRoot
     internal sealed class CompositeOrder : MonoBehaviour
     {
         [SerializeField] private PlayerCompositeRoot _playerCompositeRoot;
+        [SerializeField] private TorchCompositeRoot _torchCompositeRoot;
         
         private void Awake()
         {
             _playerCompositeRoot.Compose();
+            _torchCompositeRoot.Compose();
         }
     }
 }
