8eabff4237d1b5524de1417693b30b309d96941c
daniilkalmykov <daniilk24960@gmail.com>
2023-09-12 22:13:32 +0600

    add timer composite root

---
+++ Assets/Source/CompositeRoot/TimerCompositeRoot.cs
@@ -0,0 +1,28 @@
+using Source.TimersSystem;
+using TMPro;
+using UnityEngine;
+
+namespace Source.CompositeRoot
+{
+    internal sealed class TimerCompositeRoot : CompositeRoot
+    {
+        private TMP_Text _tmpText;
+        private Timer _timer;
+        
+        public override void Compose()
+        {
+            _tmpText = GetComponent<TMP_Text>();
+
+            _timer = new Timer();
+        }
+
+        private void Update()
+        {
+            _timer?.Update(Time.deltaTime);
+            
+            _tmpText.text = _timer!.Seconds.ToString().Length == 1
+                ? $"{_timer.Minutes} : 0{_timer.Seconds}"
+                : $"{_timer.Minutes} : {_timer.Seconds}";
+        }
+    }
+}
--- Assets/Source/TimersSystem/Timer.cs
+++ Assets/Source/TimersSystem/Timer.cs
@@ -1,12 +1,22 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Assembly-Csharp")]
 namespace Source.TimersSystem
 {
     internal sealed class Timer : ITimer
     {
+        private const int SecondsInMinutes = 60;
+        
         public float Time { get; private set; }
+        public int Seconds { get; private set; }
+        public int Minutes { get; private set; }
         
         public void Update(float deltaTime)
         {
             Time += deltaTime;
+            
+            Seconds = (int)Time - Minutes * SecondsInMinutes;
+            Minutes = (int)Time / SecondsInMinutes;
         }
     }
 }
