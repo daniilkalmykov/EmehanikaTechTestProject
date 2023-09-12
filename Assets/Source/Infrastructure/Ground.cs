057036361f24ebc1dd50e40e0ba155225d8215f8
daniilkalmykov <daniilk24960@gmail.com>
2023-09-13 00:53:29 +0600

    add ground animation

--- Assets/Source/Infrastructure/Ground.cs
+++ Assets/Source/Infrastructure/Ground.cs
@@ -4,5 +4,20 @@ namespace Source.Infrastructure
 {
     public sealed class Ground : MonoBehaviour
     {
+        [SerializeField] private float _speed;
+        [SerializeField] private Material _material;
+        
+        private void Update()
+        {
+            if (_material == null)
+            {
+                return;
+            }
+            
+            var materialMainTextureOffset = _material.mainTextureOffset;
+            materialMainTextureOffset.x -= Time.deltaTime * _speed;
+
+            _material.mainTextureOffset = materialMainTextureOffset;
+        }
     }
 }
